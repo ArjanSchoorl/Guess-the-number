394251c5f3f55d75b747db9be6e383bb413727bf
Arjan Schoorl <schoorl.arjan@outlook.com>
2019-03-02 20:32:52 +0100

    Loop with guessed numbers

--- main.py
+++ main.py
@@ -1,19 +1,40 @@
 from random import randint
-random_number = randint(0,1000)
+
 state = 0
+
+
+def integer(val):
+    try:
+        int(val)
+        return True
+    except ValueError:
+        print("This is not a number, please full in a number")
+        print("")
+        return False
 
 
 while True:
     while state == 0:
-        guess_number = input("Guess the number from 0 to 1000: ")
+        random_number = randint(0, 100)
         state = 1
+        #print(random_number)
     while state == 1:
-        if int(guess_number) > random_number:
-            print("Number " + guess_number + " is too high")
-            state = 0
-        elif int(guess_number) < random_number:
-            print("Number " + guess_number + " is too low")
-            state = 0
+        guess_number = input("Guess the number from 0 to 100: ")
+        print("")
+        if integer(guess_number):
+            if int(guess_number) > random_number:
+                print("Number " + guess_number + " is too high")
+                state = 2
+            elif int(guess_number) < random_number:
+                print("Number " + guess_number + " is too low")
+                state = 2
+            else:
+                print("You guessed the number!")
+                print("Let's try it again!")
+                print("")
+                state = 0
         else:
-            print("You guessed the number!")
-            state = 3
+            state = 1
+    while state == 2:
+        print("Try again!")
+        state = 1
