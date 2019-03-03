04d5a04c9685a0f643cce50071377c2f3b985d2f
Arjan Schoorl <schoorl.arjan@outlook.com>
2019-03-03 16:58:32 +0100

    commit

--- main.py
+++ main.py
@@ -1,40 +1,50 @@
 from random import randint
 
 state = 0
-
+x = 0 # Minimum range
+y = 100 # Maximum range
 
 def integer(val):
     try:
         int(val)
         return True
     except ValueError:
-        print("This is not a number, please full in a number")
-        print("")
+        print("This is not a number, please full in a number!")
         return False
+
+def out_of_range(val):
+    if int(val) < x or int(val) > y:
+        print("Out of range!")
+        return False
+    else:
+        return True
 
 
 while True:
     while state == 0:
-        random_number = randint(0, 100)
+        random_number = randint(x, y)
+        num_list = []
         state = 1
-        #print(random_number)
+        print(random_number)  # Debug mode
+
     while state == 1:
-        guess_number = input("Guess the number from 0 to 100: ")
-        print("")
-        if integer(guess_number):
+        guess_number = input(f"Guess the number from {x} to {y}: ")
+        if integer(guess_number) and out_of_range(guess_number):
+            num_list.append(int(guess_number))
             if int(guess_number) > random_number:
-                print("Number " + guess_number + " is too high")
-                state = 2
+                print(f"Number {guess_number} is too high")
+                print(f"Guessed numbers: {num_list}")
+                state = 1
+
             elif int(guess_number) < random_number:
-                print("Number " + guess_number + " is too low")
-                state = 2
+                print(f"Number {guess_number} is too low")
+                print(f"Guessed numbers: {num_list}")
+                state = 1
+
             else:
                 print("You guessed the number!")
-                print("Let's try it again!")
                 print("")
                 state = 0
+
         else:
             state = 1
-    while state == 2:
-        print("Try again!")
-        state = 1
